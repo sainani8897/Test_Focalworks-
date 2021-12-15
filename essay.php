2b967e70aa061753eb944f26c21043be6cf562e9
sainani8897 <sainani8897@gitlab.com>
2021-12-15 07:40:13 +0530

    test files

---
+++ essay.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>
+    Clean and healthy India
+    </h3>
+    <p>
+    It is important for India to display high standards of hygiene and cleanliness to change the overall global perception people have about our country.
+    People can make India clean in a number of ways like using a paper bag instead of a polythene to avoid littering of places. Moreover plastic bags are harmful to use.
+    Segregation of waste is also important. We should make different bins for different types of wastes. The main 3 types are - biodegradable, recyclable, and others. We should never throw away old plastic cans, we must try to reuse them
+    We must create a compost pit. Compost pit helps in the preparation of compost.Indians are very careless about waste disposal. We sure love to keep our houses clean but not our surroundings. This habit must be changed.
+    We should not throw away garbage on streets instead keep it in proper bins in a covered way
+    </p>
+</body>
+</html>
